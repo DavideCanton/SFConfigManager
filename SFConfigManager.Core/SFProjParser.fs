aee9eaf56ba076e69772181e9a84a1c784bd1acf
Davide Canton <davide.canton5@gmail.com>
2020-08-08 17:05:00 +0200

    refactoring

--- SFConfigManager.Core/SFProjParser.fs
+++ SFConfigManager.Core/SFProjParser.fs
@@ -4,8 +4,7 @@ module SFProjParser =
 
     open System.IO
     open System.Xml.Linq
-
-    let inline (!>) (x:^a) : ^b = ((^a or ^b) : (static member op_Implicit : ^a -> ^b) x)
+    open SFConfigManager.Core.Common
 
     type SFProjParser() = class
         let mutable baseFolder = ""
--- SFConfigManager.Core/SettingsParser.fs
+++ SFConfigManager.Core/SettingsParser.fs
@@ -4,8 +4,7 @@ module SettingsParser =
 
     open System.IO
     open System.Xml.Linq
-
-    let inline (!>) (x:^a) : ^b = ((^a or ^b) : (static member op_Implicit : ^a -> ^b) x)
+    open SFConfigManager.Core.Common
 
     type SettingsParser() = class
         let mutable sections = Map.empty
