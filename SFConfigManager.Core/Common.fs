771c9a15e7a69a187bd9388c922ac12e0b8125c1
Davide Canton <davide.canton5@gmail.com>
2020-08-14 17:11:13 +0200

    removed useless commons

--- SFConfigManager.Core/Common.fs
+++ SFConfigManager.Core/Common.fs
@@ -1,15 +1,5 @@
 ﻿module SFConfigManager.Core.Common
 
-open System.Xml.Linq
-
 exception InvalidFileException
-    
-let inline (!>) (x: ^a): ^b =
-    ((^a or ^b): (static member op_Implicit: ^a -> ^b) x)
-
-let getAttrValue (attributeName: string) (node: XElement) = node.Attribute(!>attributeName).Value
 
 let contains (query: string) (value: string) = value.Contains query
-
-let xNameBuilder (ns: XNamespace) name = XName.Get(name, ns.NamespaceName)
- 
