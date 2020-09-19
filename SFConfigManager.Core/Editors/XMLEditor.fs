855b3a21d9078aa90a6c4dd6392af73ebf9ad70f
Davide Canton <davide.canton5@gmail.com>
2020-09-19 10:32:22 +0200

    fixed serialization by ignoring existing whitespaces and reformatting when writing

--- SFConfigManager.Core/Editors/XMLEditor.fs
+++ SFConfigManager.Core/Editors/XMLEditor.fs
@@ -1,7 +1,9 @@
 ﻿module SFConfigManager.Core.Editors.XMLEditor
 
 open System.Xml.Linq
+open System.Text
 open System.Xml
+open System.IO
 open System.Xml.XPath
 open SFConfigManager.Core.Common
 open SFConfigManager.Extensions.ResultComputationExpression
@@ -53,27 +55,44 @@ let findElementsByXPath (xpath: string) (root: XElement) =
     List.ofSeq elements
 
 let setAttributeByXPath (path: string) (name: string) (value: string) (root: XElement) =
-    let body () =
-        let element = findElementByXPath path root
-        element.Value.SetAttributeValue(!?name, value)
-
-    protectAndRun body
+    match findElementByXPath path root with
+    | Some element -> element.SetAttributeValue(!?name, value) |> Ok
+    | None -> ErrorInActionException "Tag not found" |> Error
 
 let saveXML (path: string) (element: XElement) =
+    let makeWriter (path: string) =
+        let settings = XmlWriterSettings()
+        settings.Indent <- true
+        settings.NewLineHandling <- NewLineHandling.Replace
+        settings.Encoding <- Encoding.UTF8
+        settings.OmitXmlDeclaration <- false
+        XmlWriter.Create(path, settings)
+
+    let elementWithoutWhitespaces (element: XElement) =
+        use fs = new StringReader(element.ToString())
+        XDocument.Load(fs).Root
+
     let body () =
-        using (XmlWriter.Create(path)) (fun w -> element.Save(w))
+        using (makeWriter path) (fun w -> 
+            let el = elementWithoutWhitespaces element    
+            el.Save(w)
+        )
 
     protectAndRun body
 
 let private processAction (root: XElement) action =
     match action with
-    | SetAttribute { Path = path; Name = name; Value = value } -> setAttributeByXPath path name value root
+    | SetAttribute { Path = path; Name = name; Value = value } -> 
+        setAttributeByXPath path name value root
+
     | AddSibling { Path = path; Element = element } ->
         match findElementByXPath path root with
         | Some elFound -> addTagAfter elFound element |> Ok
         | None -> ErrorInActionException "Tag not found" |> Error
+    
     | AddSiblingElement { Target = target; Element = element} ->
         addTagAfter target element |> Ok
+    
     | AddLastChild { Path = path; Element = element } ->
         match findElementByXPath path root with
         | Some elFound -> addLastChild elFound element |> Ok
